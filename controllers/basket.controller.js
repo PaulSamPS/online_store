81fd20120b644087fdd0ff032f25d2c1b4680d25
Pavel Samoylenko <psamoylenko2@gmail.com>
2022-10-16 02:04:06 +0300

    добавление количества и сумарной стоимости

--- controllers/basket.controller.js
+++ controllers/basket.controller.js
@@ -25,12 +25,27 @@ class BasketController {
   }
 
   async addToBasket(req, res) {
-    const { productId } = req.body
+    const { productId, productPrice } = req.body
+
     if (req.signedCookies.basketId) {
       const basket = await Basket.findById(req.signedCookies.basketId).populate('products.product')
+      const product = basket.products.find((p) => p.product._id.toString() === productId)
 
-      basket.products.unshift({ product: productId, qty: 1 })
-      await basket.save()
+      if (basket.products.map((p) => p.product._id.toString()).includes(productId)) {
+        product.qty += 1
+        basket.totalPrice += product.product.price
+        await basket.save()
+      } else {
+        basket.products.unshift({ product: productId, qty: 1 })
+        basket.totalPrice += productPrice
+        await basket.save()
+      }
+      return res.json(basket)
+    } else {
+      let created = await Basket.create({ products: [], totalPrice: 0 })
+      basketId = created._id
+      res.cookie('basketId', basketId, { maxAge, signed })
+      const basket = await Basket.findById(basketId).populate('products')
       return res.json(basket)
     }
   }
