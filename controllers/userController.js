f540119ecc43811c84ad59c80f9e84c73a44515c
PaulSamPS <19071990Pqw>
2022-11-10 11:59:59 +0500

    .

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,13 +21,44 @@ const generateJwt = (payload) => {
 
 class UserController {
   async get(req, res, next) {
-    try {
-      const token = cookieParser.signedCookie(req.headers.accesstoken, process.env.SECRET_COOKIE)
-      const validateToken = await tokenService.validateAccessToken(token)
+    const accessToken = cookieParser.signedCookie(req.headers.accesstoken, process.env.SECRET_COOKIE)
+    const refreshToken = cookieParser.signedCookie(req.headers.refreshtoken, process.env.SECRET_COOKIE)
+    if (!accessToken && !refreshToken) {
+      return next(ApiError.unauthorized('Токены просрочены'))
+    }
 
-      return res.json(validateToken)
-    } catch (e) {
-      return next(ApiError.unauthorized('Не авторизован'))
+    if (!accessToken && refreshToken) {
+      const validateToken = await tokenService.validateRefreshToken(refreshToken)
+      if (validateToken) {
+        if (Date.now() >= validateToken.exp * 1000) {
+          return next(ApiError.unauthorized('Refresh токен просрочен'))
+        }
+        const token = generateJwt({
+          id: validateToken.id,
+          phone: validateToken.phone,
+          firstName: validateToken.firstName,
+          lastName: validateToken.lastName,
+          role: validateToken.role,
+        })
+        return res.json({ token: token.accessToken })
+      }
+    }
+
+    if (accessToken) {
+      const validateToken = await tokenService.validateAccessToken(accessToken)
+      if (validateToken) {
+        if (Date.now() >= validateToken.exp * 1000) {
+          return next(ApiError.unauthorized('Токен просрочен'))
+        }
+        const token = generateJwt({
+          id: validateToken.id,
+          phone: validateToken.phone,
+          firstName: validateToken.firstName,
+          lastName: validateToken.lastName,
+          role: validateToken.role,
+        })
+        return res.json({ token: token.accessToken })
+      }
     }
   }
 
