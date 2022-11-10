d4559b175d74e58cccd837ca64bd6b49ee623bab
PaulSamPS <19071990Pqw>
2022-11-10 06:53:19 +0500

    изменил middleware

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,8 +3,6 @@ const router = new Router()
 const userController = require('../controllers/userController')
 const authMiddleware = require('../middleware/authMiddleware')
 
-router.post('/registration', userController.registration)
 router.get('/login', userController.login)
-router.post('/refresh-token', userController.check)
 
 module.exports = router
